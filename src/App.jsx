2673feb77c1e05d3a7c4796ac424ce07e04c93dc
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-22 04:30:44 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import ForgotPassword from './components/auth/ForgotPassword';
+import ResetPassword from './components/auth/ResetPassword';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
- 
-    </Routes>
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword/>} />
+    </Routes>  
   );
 }
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -10,5 +10,14 @@ const login = async(email,password)=>{
     const response=await axios.post(`${API_URL}/login`,{email,password});
     return response.data;
 }
-
-export default {register,login};
+const forgotPassword = async (email) => {
+    const response = await axios.post(`${API_URL}/forgot-password`, { email });
+    return response.data;
+  };
+  
+  const resetPassword = async (token, newPassword) => {
+    const response = await axios.post(`${API_URL}/reset-password`, { token, newPassword });
+    return response.data;
+  };
+export default {register,login, forgotPassword,
+    resetPassword};
